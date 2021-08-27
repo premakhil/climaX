1691317acb08996c1d0767db7b52f42b56078f5a
Akhil Prem <akhilpchand@gmail.com>
2021-08-27 21:16:20 +0530

    Invalid message added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
+
 function App(props) {
 
     const axios = require('axios');
@@ -17,10 +18,11 @@ function App(props) {
     const [wind, setWind] = useState('');
     const [pressure, setPressure] = useState('');
     const [country, setCountry] = useState('');
+    const [invalid, setInvalid] = useState(false);
 
 
 
-    // const url = 'https://api.openweathermap.org/data/2.5/weather?q=Kannur&units=metric&APPID=202ea05f1ea260ad09ef2d90fe39df3a';
+
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + props.search + '&units=metric&APPID=202ea05f1ea260ad09ef2d90fe39df3a';
 
     const getData = () => {
@@ -36,13 +38,25 @@ function App(props) {
             setHumidity(response.data.main.humidity);
             setCountry(response.data.sys.country);
 
+            setInvalid(false);
+
 
 
 
 
         })
-            .catch(error => console.error(error));
 
+
+            .catch((error) => {
+                console.error(error);
+                setInvalid(true);
+
+
+
+
+
+
+            })
     }
 
 
@@ -55,25 +69,27 @@ function App(props) {
 
 
     return (
-        <div className="main-class">
+        <div>
+            {invalid ? <h1>Invalid entry</h1> : <div className="main-class">
 
 
 
 
-            <h1>{name}</h1> <br />
+                <h1>{name}</h1> <br />
 
-            <h2>{temp}</h2>
-            <h2>{maxtemp}</h2>
-            <h2>{mintemp}</h2>
-            <h2>{humidity}</h2>
-            <h2>{pressure}</h2>
-            <h2>{wind}</h2>
-            <h2>{country}</h2>
-
-
+                <h2>{temp}</h2>
+                <h2>{maxtemp}</h2>
+                <h2>{mintemp}</h2>
+                <h2>{humidity}</h2>
+                <h2>{pressure}</h2>
+                <h2>{wind}</h2>
+                <h2>{country}</h2>
 
 
 
+
+
+            </div>}
         </div>
     )
 }
